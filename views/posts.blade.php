0f0964dbc5e87a295b51d4a5c651ec8467482fea
joshuapras <97777283+joshuapras@users.noreply.github.com>
2022-02-06 13:54:33 +0700

    Add files via upload

---
+++ views/posts.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layouts.main')
+
+@section('container')
+
+ @foreach($posts as $post)
+<article class="mb-5">
+ <h2>
+     <a href="/posts/{{ $post->id }} " >{{ $post->title  }}</a>
+    </h2>
+ <p>{{$post->excerpt }}</p>
+ </article>
+ @endforeach
+
+
+@endsection
+
